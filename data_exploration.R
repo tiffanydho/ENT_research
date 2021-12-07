183bfd8fb249c1f2dc49e509cb4895bddd7dc0b4
Tiffany Ho <tiffany.d.ho@gmail.com>
2021-12-06 22:00:50 -0500

    create processed_data tibble, as opposed to modifying the raw_data tibble

--- data_exploration.R
+++ data_exploration.R
@@ -1,5 +1,5 @@
 #install.packages("pacman")
-pacman::p_load(readxl, dplyr, tidyr, tidyselect)
+pacman::p_load(readxl, dplyr, tidyr, tidyselect, tibble)
 # library(readxl)
 # library(dplyr)
 # library(tidyr)
@@ -12,34 +12,49 @@ raw_data <- read_excel(file.path("data",filename), sheet = 1,
                        col_names = TRUE, col_types = NULL, na = "", skip = 0)
 raw_data <- raw_data %>% 
   rename(DOB="Date of birth", allergies="List of allergies (separate by commas)")
-raw_data <- raw_data %>% 
-  mutate(Age = calcAge(as.Date(DOB))) %>% relocate(Age, .after = DOB)
-raw_data <- raw_data %>% 
-  mutate(num_allergies = countItem(allergies)) %>% relocate(num_allergies, .before = allergies)
 
-raw_data <- raw_data %>% 
-  mutate(R_endo_score=rowSums(across(vars_select(names(raw_data), starts_with("Right endoscopy", ignore.case = TRUE), -contains("total"))))) %>% 
-  relocate(R_endo_score, .after = "Right endoscopy total score")
-raw_data <- raw_data %>% 
-  mutate(L_endo_score=rowSums(across(vars_select(names(raw_data), starts_with("Left endoscopy", ignore.case = TRUE), -contains("total"))))) %>% 
-  relocate(L_endo_score, .after = "Left endoscopy total score")
-raw_data <- raw_data %>% 
-  mutate(endo_score=rowSums(across(vars_select(names(raw_data), one_of("R_endo_score", "L_endo_score"))))) %>% 
-  relocate(endo_score, .after = "R_endo_score")
+# omit the following categorical vars for now - will train with ML later
+## gender, race, ethnicity, diagnoses
+processed_data <- raw_data[, c("Study ID")]
 
-raw_data <- raw_data %>% mutate_at(c(113:142), as.numeric) # to figure out str -> var name (instead of hard-coding by index)
-raw_data <- raw_data %>% 
-  mutate(survey_score_cat1=rowMeans(subset(raw_data, select=c(113:142)), na.rm = TRUE)) %>% 
-  relocate(survey_score_cat1, .after = "My sexual activity is affected by my problem")
-# omit cat2 responses for now - too  many missing values
+processed_data <- processed_data %>% 
+  mutate(age=calcAge(as.Date(raw_data$DOB))) %>% 
+  mutate(num_allergies = countItem(raw_data$allergies)) %>%
+  mutate(R_endo_score=rowSums(select(raw_data, starts_with("Right endoscopy", ignore.case = TRUE), -contains("total")))) %>% 
+  mutate(L_endo_score=rowSums(select(raw_data, starts_with("Left endoscopy", ignore.case = TRUE), -contains("total")))) 
+processed_data <- processed_data %>% 
+  mutate(endo_score=rowSums(select(processed_data, one_of("R_endo_score", "L_endo_score"))))
 
-#cat3: col 151 - 170 (NOT reliable, must change)
-factor(score, levels = c("0 never", "1 hardly", "2 some of the time", "3 often", "4 most of the time") , labels = c("0", "1", "2", "3", "4"))
+# cat1: col 108-137
+cat1_col <- c(108:137)
+raw_data <- raw_data %>% mutate_at(cat1_col, as.numeric)
+processed_data <- processed_data %>%
+  mutate(survey_score_cat1=rowMeans(subset(raw_data, select=cat1_col), na.rm = TRUE)) 
+
+# middle group (technically group 2) was omitted due to large amount of missing value
+
+# cat2: col 145-165 
 # score <- c("4 most of the time", "4 most of the time", "1 hardly")
 # score_f <- factor(score, levels = c("0 never", "1 hardly", "2 some of the time", "3 often", "4 most of the time") , labels = c("0", "1", "2", "3", "4"))
 # as.numeric(as.character(score_f))
-raw_data_test <- raw_data %>% mutate_at(c(151:170), ~factor(.x, levels = c("0 never", "1 hardly", "2 some of the time", "3 often", "4 most of the time") , labels = c("0", "1", "2", "3", "4")))
-raw_data_test1 <- raw_data %>% mutate_at(raw_data, vars(Gender,Race), ~factor(.x, levels = c("0 never", "1 hardly", "2 some of the time", "3 often", "4 most of the time") , labels = c("0", "1", "2", "3", "4")))
+
+cat2_col <- c(145:164)
+raw_data <- raw_data %>% 
+  mutate_at(cat2_col, ~factor(.x, levels = c("0 never", "1 hardly", "2 some of the time", "3 often", "4 most of the time"), labels = c("0", "1", "2", "3", "4"))) %>%  
+  mutate_at(cat2_col, as.character) %>%   
+  mutate_at(cat2_col, as.numeric)
+processed_data <- processed_data %>%
+  mutate(survey_score_cat2=rowMeans(subset(raw_data, select=cat2_col), na.rm = TRUE)) 
+
+cat3_col <- c(166:177)
+raw_data <- raw_data %>% 
+  mutate_at(cat3_col, ~factor(.x, levels = c("1 Extremely Uncharacteristic", "2", "3", "4", "5 Extremely Characteristic"), labels = c("1","2","3","4","5"))) %>% 
+  mutate_at(cat3_col, as.character) %>%   
+  mutate_at(cat3_col, as.numeric)
+processed_data <- processed_data %>%
+  mutate(survey_score_cat3=rowMeans(subset(raw_data, select=cat3_col), na.rm = TRUE)) 
+
+
 # to do:
 # - calc average survery scores (by category)
 # - change diagnoses/drug use to BOOLEAN (unchecked -> F, checked -> T)
