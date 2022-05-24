b70b3e0d8afbf8dd6a76c79e6c2f93144a75ba1a
Tiffany Ho <tiffany.d.ho@gmail.com>
2022-05-24 00:02:09 -0400

    sex bias & next step script

--- ERAS_Linguistics/ERAS_script_race.R
+++ ERAS_Linguistics/ERAS_script_race.R
@@ -9,35 +9,11 @@ filename <- "Data-LOR-trimmed.xlsx"
 # year 2019-20: sheet 12 & 13
 
 ### COMBINE TWO CYCLES: 2019-20 & 2020-21 ###
-data_f_20 <- read_excel(filename, sheet = 1, col_names = TRUE, col_types = NULL, na = "", skip = 0)
-data_m_20 <- read_excel(filename, sheet = 2, col_names = TRUE, col_types = NULL, na = "", skip = 0)
-data_f_19 <- read_excel(filename, sheet = 12, col_names = TRUE, col_types = NULL, na = "", skip = 0)
-data_m_19 <- read_excel(filename, sheet = 13, col_names = TRUE, col_types = NULL, na = "", skip = 0)
-data <- rbind(data_f_20, data_m_20, data_f_19, data_m_19) %>%
-  rename(step_1 = "Step 1", step_2 = "Step 2", sex = "Sex", race = "Self-Identification 1",
-         peer_art_abs = "Peer Reviewed Journal Articles/Abstracts",
-         extra_art_abs = "(Other than Published) Peer Reviewed Journal Articles/Abstracts",
-         book_chap = "Book Chapter (Peer Reviewed)", poster = "Poster Presentation",
-         oral = "Oral Presentation", online_pub_peer = "Online Publication (Peer Reviewed)",
-         online_pub_non_peer = "Non Peer Reviewed Online Publication",
-         other_art = "Other Articles") %>%
-  replace_with_na_all(condition = ~.x =="None") %>%
-  filter(!is.na(race)) %>% 
-  mutate(step_1 = as.numeric(step_1), step_2 = as.numeric(step_2)) %>%
-  mutate(race = gsub("Asian", "Asian", .$race, ignore.case = TRUE)) %>% 
-  mutate(race = gsub("^.*black.*$", "Black", .$race, ignore.case = TRUE)) %>%
-  mutate(race = gsub("^.*hispanic.*$", "Hispanic", .$race, ignore.case = TRUE)) %>% 
-  mutate(race = gsub("^.*egyptian.*$|^.*iranian.*$|^.*middle[ |-]eastern.*$|other", 
-                     "Other", .$race, ignore.case = TRUE ))
-
-# GOALS:
-# step 1 & 11 subjective variables: control for step 1
-# both years separately & combined years
-
-### INDIVIDUAL CYCLE ###
-# data_f <- read_excel(filename, sheet = 12, col_names = TRUE, col_types = NULL, na = "", skip = 0)
-# data_m <- read_excel(filename, sheet = 13, col_names = TRUE, col_types = NULL, na = "", skip = 0)
-# data <- rbind(data_f, data_m) %>%
+# data_f_20 <- read_excel(filename, sheet = 1, col_names = TRUE, col_types = NULL, na = "", skip = 0)
+# data_m_20 <- read_excel(filename, sheet = 2, col_names = TRUE, col_types = NULL, na = "", skip = 0)
+# data_f_19 <- read_excel(filename, sheet = 12, col_names = TRUE, col_types = NULL, na = "", skip = 0)
+# data_m_19 <- read_excel(filename, sheet = 13, col_names = TRUE, col_types = NULL, na = "", skip = 0)
+# data <- rbind(data_f_20, data_m_20, data_f_19, data_m_19) %>%
 #   rename(step_1 = "Step 1", step_2 = "Step 2", sex = "Sex", race = "Self-Identification 1",
 #          peer_art_abs = "Peer Reviewed Journal Articles/Abstracts",
 #          extra_art_abs = "(Other than Published) Peer Reviewed Journal Articles/Abstracts",
@@ -54,6 +30,27 @@ data <- rbind(data_f_20, data_m_20, data_f_19, data_m_19) %>%
 #   mutate(race = gsub("^.*egyptian.*$|^.*iranian.*$|^.*middle[ |-]eastern.*$|other", 
 #                      "Other", .$race, ignore.case = TRUE ))
 
+
+### INDIVIDUAL CYCLE ###
+data_f <- read_excel(filename, sheet = 12, col_names = TRUE, col_types = NULL, na = "", skip = 0)
+data_m <- read_excel(filename, sheet = 13, col_names = TRUE, col_types = NULL, na = "", skip = 0)
+data <- rbind(data_f, data_m) %>%
+  rename(step_1 = "Step 1", step_2 = "Step 2", sex = "Sex", race = "Self-Identification 1",
+         peer_art_abs = "Peer Reviewed Journal Articles/Abstracts",
+         extra_art_abs = "(Other than Published) Peer Reviewed Journal Articles/Abstracts",
+         book_chap = "Book Chapter (Peer Reviewed)", poster = "Poster Presentation",
+         oral = "Oral Presentation", online_pub_peer = "Online Publication (Peer Reviewed)",
+         online_pub_non_peer = "Non Peer Reviewed Online Publication",
+         other_art = "Other Articles") %>%
+  replace_with_na_all(condition = ~.x =="None") %>%
+  filter(!is.na(race)) %>%
+  mutate(step_1 = as.numeric(step_1), step_2 = as.numeric(step_2)) %>%
+  mutate(race = gsub("Asian", "Asian", .$race, ignore.case = TRUE)) %>%
+  mutate(race = gsub("^.*black.*$", "Black", .$race, ignore.case = TRUE)) %>%
+  mutate(race = gsub("^.*hispanic.*$", "Hispanic", .$race, ignore.case = TRUE)) %>%
+  mutate(race = gsub("^.*egyptian.*$|^.*iranian.*$|^.*middle[ |-]eastern.*$|other",
+                      "Other", .$race, ignore.case = TRUE ))
+
 data$race <- factor(data$race) 
 summary(data$race)
 
@@ -64,7 +61,7 @@ for(col.name in colnames(data[c(2,3, 15:25)]) ){
                             "-------------", sep = " "))
   mylist <- c(mylist, tapply(data[[col.name]], data$race, summary) )
 }
-capture.output(mylist, file = "ERAS_race_variable_summaries.txt")
+capture.output(mylist, file = "ERAS_race_variable_summaries_2019-20_LOR.txt")
 
 
 # data %>% group_by(race) %>% get_summary_stats(step_1, type = "mean_sd")
@@ -208,7 +205,7 @@ for(col.name in all.col.names){
   boxplot_list[[i]] = p      
 }
 
-pdf("ERAS_race_boxplots_combined_LOR.pdf")
+pdf("ERAS_race_boxplots_2019-20_LOR.pdf")
 for (i in 1:length(all.col.names)) {
   print(boxplot_list[[i]])
 }
